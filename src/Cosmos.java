fd385b47053f1859f219cc9498c02302a7531aa6
Patryk Likus <mlodyasg1@gmail.com>
2020-09-10 11:06:49 +0200

    add window

--- src/Cosmos.java
+++ src/Cosmos.java
@@ -1,8 +1,23 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Cosmos extends JPanel {
 
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(1000, 1000);
+    }
+
     public static void main(String[] args) {
-	
+
+        JFrame window = new JFrame("Cosmos");
+        Cosmos cosmos = new Cosmos();
+
+        window.add(cosmos);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLocation(480, 5);
+        window.setVisible(true);
+        window.pack();
     }
 }
